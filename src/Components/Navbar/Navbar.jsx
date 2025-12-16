182f2d0f7b974d3358de356f35b0701581399697
Mohammed Elalimy <mohammed.m.elalimy@gmail.com>
2025-12-16 02:39:59 +0200

    updates

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -25,29 +25,29 @@ const Navbar = () => {
             <ul ref={menuRef} className="nav-menu">
                 <img src={closed} alt='closed' className='nav-mob-closed' onClick={closeMenu}/>
             <li>
-                <AnchorLink className='anchor-link' >
-                    <p onClick={() => setMenu('home')}>Menu</p>
-                </AnchorLink>
+            <AnchorLink className="anchor-link">
+                <p onClick={() => {setMenu('home'); closeMenu();}}>Menu</p>
+            </AnchorLink>
                 {menu === 'home' && <img src={underlined} alt="" />}
             </li>
 
             <li>
             <AnchorLink className='anchor-link' offset={50} href='#about'>
-                <p onClick={() => setMenu('about')}>About Me</p>
+                        <p onClick={() => { setMenu('about'); closeMenu(); }}>About Me</p>
             </AnchorLink>
             {menu === 'about' && <img src={underlined} alt="" />}
             </li>
 
             <li>
             <AnchorLink className='anchor-link' offset={50} href='#work'>
-                <p onClick={() => setMenu('work')}>Portfolio</p>
+                <p onClick={() => { setMenu('work'); closeMenu(); }}>Portfolio</p>
             </AnchorLink>
             {menu === 'work' && <img src={underlined} alt="" />}
             </li>
 
             <li>
             <AnchorLink className='anchor-link' offset={50} href='#contact'>
-                <p onClick={() => setMenu('contact')}>Contact</p>
+                <p onClick={() => { setMenu('contact'); closeMenu(); }}>Contact</p>
             </AnchorLink>
             {menu === 'contact' && <img src={underlined} alt="" />}
             </li>
