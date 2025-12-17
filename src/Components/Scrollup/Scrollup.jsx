ddf1c250ec15f68610a710eeeb34f788b2ac5dcd
Mohammed Elalimy <mohammed.m.elalimy@gmail.com>
2025-12-17 16:20:15 +0200

    init

--- src/Components/Scrollup/Scrollup.jsx
+++ src/Components/Scrollup/Scrollup.jsx
@@ -1,5 +1,5 @@
     import React, { useState, useEffect } from "react";
-    import './ScrollUp.css';
+    import './Scrollup.css';
 
     const ScrollUp = () => {
     const [visible, setVisible] = useState(false);
