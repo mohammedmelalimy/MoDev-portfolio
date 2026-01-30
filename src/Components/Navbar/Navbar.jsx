210e4485caf5bd2eb593345eadb52a357fbb449d
Mohammed Elalimy <mohammed.m.elalimy@gmail.com>
2026-01-30 20:47:18 +0200

    enhance navbar style

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -20,7 +20,9 @@ const Navbar = () => {
 
     // 🌟 Determine active nav item based on route path
     const currentPath = location.pathname; 
-
+    const scrollUp = () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    };
     return (
         <div className="navbar">
 
@@ -29,41 +31,41 @@ const Navbar = () => {
                 <p>Elalimy</p>
             </Link>
 
-            <img src={toggle} alt="toggle" className="nav-mob-open" onClick={openMenu} />
+            <img src={toggle} alt="toggle" className="nav-mob-open" onClick={() => { openMenu(); scrollUp(); }} />
 
             <ul ref={menuRef} className="nav-menu">
                 <img src={closed} alt="closed" className="nav-mob-closed" onClick={closeMenu} />
 
                 <li>
-                    <Link to="/" onClick={closeMenu}>
+                    <Link to="/" onClick={() => { closeMenu(); scrollUp(); }}>
                         <p>Menu</p>
                     </Link>
                     {currentPath === '/' && <img src={underlined} alt="active" />}
                 </li>
 
                 <li>
-                    <Link to="/about" onClick={closeMenu}>
+                    <Link to="/about" onClick={() => { closeMenu(); scrollUp(); }}>
                         <p>About Me</p>
                     </Link>
                     {currentPath === '/about' && <img src={underlined} alt="active" />}
                 </li>
 
                 <li>
-                    <Link to="/projects" onClick={closeMenu}>
+                    <Link to="/projects" onClick={() => { closeMenu(); scrollUp(); }}>
                         <p>Projects</p>
                     </Link>
                     {currentPath === '/projects' && <img src={underlined} alt="active" />}
                 </li>
 
                 <li>
-                    <Link to="/contact" onClick={closeMenu}>
+                    <Link to="/contact" onClick={() => { closeMenu(); scrollUp(); }}>
                         <p>Contact</p>
                     </Link>
                     {currentPath === '/contact' && <img src={underlined} alt="active" />}
                 </li>
             </ul>
 
-            <Link to="/contact" className="nav-connect" onClick={closeMenu}>
+            <Link to="/contact" className="nav-connect" onClick={() => { closeMenu(); scrollUp(); }}>
                 <p>Connect With Me</p>
             </Link>
         </div>
