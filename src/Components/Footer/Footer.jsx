f0355a9daa59d78503e6ee7e1eb2f3902204f671
Mohammed Elalimy <mohammed.m.elalimy@gmail.com>
2026-02-01 10:08:55 +0200

    modify footer background and width

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,41 +1,42 @@
-    import React from 'react'
-    import './Footer.css'
-    import Logo from '../../assets/logo.svg'
+    import Logo from '../../assets/logo.svg';
+import './Footer.css';
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
+      <div className='container'>
         <div className="footer">
-        <div className="footer-top">
-            <div className="footer-top-left">
-            <img src={Logo} alt='logo' className="footer-logo" />
-            <p>
-                I am a frontend developer passionate about building modern and
-                user-friendly web experiences.
-            </p>
-            </div>
-            <div className="footer-top-right">
-                <a href="https://www.facebook.com/profile.php?id=100040613604023" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-facebook-f"></i>
-                </a>
-                <a href="https://github.com/mohammedmelalimy" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-github"></i>
-                </a>
-                <a href="https://www.linkedin.com/in/mohammed-elalimy-aa2556287" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-linkedin"></i>
-                </a>
-                <a href="https://wa.me/201113414805" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-whatsapp"></i>
-                </a>
-            </div>
+          <div className="footer-top">
+              <div className="footer-top-left">
+              <img src={Logo} alt='logo' className="footer-logo" />
+              <p>
+                  I am a frontend developer passionate about building modern and
+                  user-friendly web experiences.
+              </p>
+              </div>
+              <div className="footer-top-right">
+                  <a href="https://www.facebook.com/profile.php?id=100040613604023" target="_blank" rel="noopener noreferrer">
+                      <i className="fa-brands fa-facebook-f"></i>
+                  </a>
+                  <a href="https://github.com/mohammedmelalimy" target="_blank" rel="noopener noreferrer">
+                      <i className="fa-brands fa-github"></i>
+                  </a>
+                  <a href="https://www.linkedin.com/in/mohammed-elalimy-aa2556287" target="_blank" rel="noopener noreferrer">
+                      <i className="fa-brands fa-linkedin"></i>
+                  </a>
+                  <a href="https://wa.me/201113414805" target="_blank" rel="noopener noreferrer">
+                      <i className="fa-brands fa-whatsapp"></i>
+                  </a>
+              </div>
+          </div>
+          <hr />
+          <div className="footer-bottom">
+              <p className="footer-bottom-left">
+              © {currentYear} Mohammed Elalimy. All rights reserved.
+              </p>
+          </div>
         </div>
-        <hr />
-        <div className="footer-bottom">
-            <p className="footer-bottom-left">
-            © {currentYear} Mohammed Elalimy. All rights reserved.
-            </p>
-        </div>
-        </div>
+      </div>
     )
     }
 
